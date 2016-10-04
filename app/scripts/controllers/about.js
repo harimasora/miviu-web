ea5f7cf574cc93151547400be48e9d9cb3c71453
Danilo Maia Rodrigues <zidalo@gmail.com>
2016-10-04 09:40:13 -0300

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miviuApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the miviuApp
+ */
+angular.module('miviuApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miviuApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the miviuApp
+ */
+angular.module('miviuApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
